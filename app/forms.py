671bc04bb426978f9fa625cd3401dfa4ea72f5a9
SheamarWhite <sheam2002@LAPTOP-52FS6K8S>
2024-03-06 16:33:25 -0500

    Second Commit

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, FileField
+from falsk_wtf.file import FileRequired, FileAllowed
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    photo = FileField('image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
